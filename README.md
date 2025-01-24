ae86df86a7ba75e6a1f06dd3418f0a53702a9f5e
Maratib Ali Khan <maratib@gmail.com>
2025-01-24 20:11:32 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Java Starter
+
+with
+
+- Java 11
+- JUnit 5
+- Maven JAR plugin
+- VSCode test settings
+
+```bash
+# To create jar file run
+mvn package
+# or
+mvn clean package
+```
---
+++ src/test/java/Tests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    public void test1() {
+        System.out.println("Hello from Test1");
+    }
+}
